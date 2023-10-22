395d6385900dc22c21e16a5b14a42f3ce12a63cb
gosha <goshaapolonoff@yandex.ru>
2023-10-22 16:39:58 +0300

    Algorithm's bug fixed

--- src/main/java/ru/vsu/cs/zagorodnev_g_a/AlgorithmWuController.java
+++ src/main/java/ru/vsu/cs/zagorodnev_g_a/AlgorithmWuController.java
@@ -3,6 +3,8 @@ package ru.vsu.cs.zagorodnev_g_a;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 
@@ -11,11 +13,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.transform.Scale;
 
 public class AlgorithmWuController {
-
     @FXML
     AnchorPane anchorPane;
     @FXML
-    private Canvas canvas;
+    Canvas canvas;
     @FXML
     private Slider sliderX0;
     @FXML
@@ -24,7 +25,10 @@ public class AlgorithmWuController {
     private Slider sliderX1;
     @FXML
     private Slider sliderY1;
+
     WuLine wuLine = new WuLine();
+
+    private double x0,y0,x1,y1;
     @FXML
     private void initialize() {
         anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
@@ -50,6 +54,9 @@ public class AlgorithmWuController {
 
         draw();
 
+        canvas.setOnMousePressed(this::handleMouseDown);
+        canvas.setOnMouseReleased(this::handleMouseUp);
+
         sliderX0.valueProperty().addListener((observable, oldValue, newValue) -> {
             draw();
         });
@@ -74,10 +81,27 @@ public class AlgorithmWuController {
         double y0 = sliderY0.getValue();
         double x1 = sliderX1.getValue();
         double y1 = sliderY1.getValue();
-        wuLine.drawLine(canvas.getGraphicsContext2D(), x0,y0,x1,y1);
-        wuLine.drawLine(canvas.getGraphicsContext2D(), 400,500,500,300);
+        wuLine.drawLine(canvas.getGraphicsContext2D(),x0,y0,x1,y1);
 
         canvas.getGraphicsContext2D().restore();
     }
 
+    private void handleMouseDown(MouseEvent event) {
+        if (event.getButton() == MouseButton.PRIMARY) {
+            x0 = event.getX();
+            y0 = event.getY();
+        }
+    }
+
+    private void handleMouseUp(MouseEvent event) {
+        if (event.getButton() == MouseButton.PRIMARY) {
+            x1 = event.getX();
+            y1 = event.getY();
+
+            drawWuLine();
+        }
+    }
+    private void drawWuLine() {
+        wuLine.drawLine(canvas.getGraphicsContext2D(), x0,y0,x1,y1);
+    }
 }
